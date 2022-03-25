7799b6521bc7d89102a61120af94372cb269243b
Víctor Salinas <47522729+salinasdev@users.noreply.github.com>
2022-03-25 13:20:52 +0100

    Add files via upload

---
+++ Cobol/8kyu/multiply.cbl
@@ -0,0 +1,12 @@
+       identification division.
+       program-id. f01-mult.
+       data division.
+       working-storage section.
+       linkage section.
+       01  num-1        pic 9(4).
+       01  num-2        pic 9(4).
+       01  result       pic 9(8).
+       procedure division using num-1 num-2 result.
+           MULTIPLY num-1 by num-2 giving result  
+           .
+       end program f01-mult.
