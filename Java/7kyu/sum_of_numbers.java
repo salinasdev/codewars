ce9a8ed73f16d3e779aafa2f47b70983ea48ca42
Víctor Salinas <47522729+salinasdev@users.noreply.github.com>
2022-06-14 12:41:59 +0200

    Add files via upload

---
+++ Java/7kyu/sum_of_numbers.java
@@ -0,0 +1,14 @@
+  //Hecho por salinasdev
+  //https://www.codewars.com/kata/55f2b110f61eb01779000053/train/java
+  public class Sum
+  {
+     public int GetSum(int a, int b)
+     {
+      if (a == b){
+        return 1;
+        }else{
+        return a + b;
+        }
+     
+     }
+  }
